dc2ca3cf2b3cfe2d5631a7898e0ac64783935492
Franco Curotto <francocurotto@gmail.com>
2019-12-16 12:59:40 -0300

    trying to initialize the package correctly

---
+++ README.md
@@ -0,0 +1,3 @@
+# calandigital
+
+Helper package to ease the development of CASPER's ROACH1 and ROACH2 scripts.
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages, findall
+
+setup(name             = 'calandigital',
+      version          = '0.1',
+      description      = 'Helper package to ease the development of CASPER\'s ROACH1 and ROACH2 scripts.',
+      url              = 'http://github.com/francocalan/calandigital',
+      author           = 'Franco Curotto',
+      author_email     = 'francocurotto@gmail.com',
+      license          = 'GPL v3',
+      packages         = [pack.replace('source', 'calandigital') for pack in find_packages()],
+      package_dir      = {pack.replace('source', 'calandigital') : pack.replace('.', '/') for pack in find_packages()},
+      scripts          = findall('scripts/'),
+      install_requires = [line.rstrip('\n') for line in open('REQUIREMENTS') if not line.startswith('git+https://')],
+      dependency_links = [line.rstrip('\n') for line in open('REQUIREMENTS') if line.startswith('git+https://')],
+      zip_safe         = False)
