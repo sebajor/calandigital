f02fc2e16bcc296e265c84b4e1042bc53e0db803
seba <junkseba@gmail.com>
2022-04-20 09:27:38 -0400

    test the arbitrary generator codes

--- source/instruments/arbitrary_generator.py
+++ source/instruments/arbitrary_generator.py
@@ -7,7 +7,7 @@ import numpy as np
 
 class arbitrary_generator():
     
-    def __init__(self, instr_name, sleep_time):
+    def __init__(self, instr_name, sleep_time=0.1):
         """
         Class to handle an arbitrary wave generator
         instr_name  : Visa name of the generator
@@ -16,7 +16,16 @@ class arbitrary_generator():
         rm = visa.ResourceManager('@py')
         self.instr = rm.open_resource(instr_name)
         self.sleep_time = sleep_time
+        time.sleep(self.sleep_time)
+        self.set_termination()
     
+    def set_termination(self, term=50):
+        """
+        Set the output termination
+        """
+        self.instr.write('outp:load '+str(term))
+        time.sleep(self.sleep_time)
+
     def turn_output_on(self):
         """
         Turn on the output of the generator.
@@ -30,20 +39,51 @@ class arbitrary_generator():
         """
         self.instr.write('outp off')
         time.sleep(self.sleep_time)
+
+    def get_output_status(self):
+        """
+        Return true if the output is on
+        """
+        status = self.instr.query('outp?')
+        time.sleep(self.sleep_time)
+        out = (status=='1\n')
+        return out
     
     def set_freq_hz(self, freq):
         self.instr.write('freq '+str(freq))
         time.sleep(self.sleep_time)
 
+    def get_freq(self):
+        out= self.instr.query('freq?')
+        time.sleep(self.sleep_time)
+        out = float(out)
+        return out
+
+
     def set_amplitude_vpp(self, volt):
         self.instr.write('volt '+str(volt))
         time.sleep(self.sleep_time)
+    
+    def get_amplitude(self):
+        out= self.instr.query('volt?')
+        time.sleep(self.sleep_time)
+        out = float(out)
+        return out
 
     def set_offset_volt(self, volt):
         self.instr.write('volt:offs '+str(volt))
         time.sleep(self.sleep_time)
 
-    def select_waveform(self, option='sine', user_name='VOLATILE'):
+    def get_offset(self):
+        """
+        Get the voltage offset
+        """
+        out= self.instr.query('volt:offs?')
+        time.sleep(self.sleep_time)
+        out = float(out)
+        return out
+
+    def set_waveform(self, option='sine', user_name='volatile'):
         """
         select the waveform type
         option      : sine, square, ramp, pulse, noise, dc, user
@@ -53,13 +93,25 @@ class arbitrary_generator():
         waves = {'sine':'sin', 'square':'squ', 'ramp':'ramp', 'pulse':'puls',
                 'noise':'nois', 'dc':'dc', 'user':'user'}
         if(option is not 'user'):
-            self.instr.write('func: '+waves[option])
+            self.instr.write('func '+waves[option])
             time.sleep(self.sleep_time)
         else:
-            self.instr.write('func: user '+user_name)
+            self.instr.write('func user')
+            self.instr.write('func:user '+user_name)
             time.sleep(self.sleep_time)
 
-    
+    def get_waveform(self):
+        """
+            Get the current waveform
+        """
+        waves = {'SIN\n':'sine', 'SQU\n':'square', 'RAMP\n':'ramp', 'PULS\n':'pulse',
+                'NOISE\n':'noise', 'DC\n':'dc', 'USER\n':'user'}
+        status = self.instr.query('func?')
+        time.sleep(self.sleep_time)
+        out = waves[status]
+        return out
+   
+
     def sweep_config(self, sweep_time, start, stop, sweep_type='linear'):
         """
             sweep time  :   time for the sweeping in seconds
@@ -95,9 +147,8 @@ class arbitrary_generator():
         """
         if((np.abs(data)>1).any()):
             raise Exception("The data values should be normalized to (-1,1) range")
-        str_data = np.array2string(data)
+        str_data = str(data.tolist())
         str_data = str_data[1:-1]           #no consider the [ ] 
-        str_data = str_data.replace(' ',',')#put commas between values    
         self.instr.write("DATA VOLATILE,"+str_data)
         time.sleep(self.sleep_time)
         if(name is not None):
@@ -113,10 +164,13 @@ class arbitrary_generator():
         time.sleep(self.sleep_time)
         
     
-    def burst_config(self, cycles, trigger_type='immediate'):
+    def burst_config(self, cycles, trigger_type='bus'):
         """
         cycles          :   number of cycles to output per burst
         trigger_type    :   'inmediate','external','bus'
+
+        The default trigger is bus and you have to send a sw trigger using the
+        send_sw_trigger function
 
         """
         trigg = {'immediate':'IMM', 'external':'EXT','bus':'BUS'}
